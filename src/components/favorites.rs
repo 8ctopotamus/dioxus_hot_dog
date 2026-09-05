a6e1a92e707777e96de51896d0ddfee7c4af2908
Josh <zylo.codes@gmail.com>
2026-09-05 08:18:35 -0500

    fix build

--- src/components/favorites.rs
+++ src/components/favorites.rs
@@ -1,21 +1,20 @@
 use dioxus::prelude::*;
+use crate::backend;
 
 #[component]
 pub fn Favorites() -> Element {
+  let mut favorites = use_server_future(backend::list_dogs)?;
+  
   rsx! {
-    let mut favorites = use_server_future(super::backend::list_dogs)?;
-    
-    rsx! {
+    div {
+      id: "favorites",
       div {
-        id: "favorites",
-        div {
-          id: "favorites-container",
-          for (id, url) in favorites().unwrap().unwrap() {
-            div {
-              key: "{id}",
-              class: "favorite-dog",
-              img { src: "{url}" }
-            }
+        id: "favorites-container",
+        for (id, url) in favorites().unwrap().unwrap() {
+          div {
+            key: "{id}",
+            class: "favorite-dog",
+            img { src: "{url}" }
           }
         }
       }
